3657f6450204613244f65df5fd772a5dad584441
Сергей Макаров <sergey.mcarrow@gmail.com>
2018-02-23 17:45:28 +0200

    hw6

--- hw6/Program.cs
+++ hw6/Program.cs
@@ -10,6 +10,31 @@ namespace hw6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task 3");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            for (int i = num1; i < num2; i++)
+            {
+                if (i % 5 == 0)
+                    Console.WriteLine(i);
+            }
+
+
+            Console.WriteLine("Task 11");
+            ulong first = 1;
+            ulong second = 1;
+
+            int counter100 = 100;
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            while (counter100 > 0)
+            {
+                ulong fibo = first + second;
+                counter100--;
+                Console.WriteLine(fibo);
+                first = second;
+                second = fibo;
+            }
         }
     }
 }
